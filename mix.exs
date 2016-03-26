eaadf5985ce069625cf58f387bbbf992a502dbb7
Catalin Mititiuc <catalin.mititiuc@gmail.com>
2016-03-26 12:30:32 -0400

    Update deps

--- mix.exs
+++ mix.exs
@@ -30,10 +30,10 @@ defmodule HelloPhoenix.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.0.3"},
-     {:phoenix_ecto, "~> 1.1"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.1"},
+    [{:phoenix, "~> 1.1"},
+     {:phoenix_ecto, "~> 2.0"},
+     {:postgrex, "~> 0.7"},
+     {:phoenix_html, "~> 2.5"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"}]
   end
