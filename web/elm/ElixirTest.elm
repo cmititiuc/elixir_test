e01ea69716934412ffe3a752441ff8fb3ba4f907
Catalin Mititiuc <catalin.mititiuc@gmail.com>
2016-03-27 17:28:17 -0400

    Refactor

--- web/elm/ElixirTest.elm
+++ web/elm/ElixirTest.elm
@@ -2,54 +2,56 @@ module ElixirTest where
 
 import Color exposing (..)
 import Graphics.Collage exposing (..)
+import Graphics.Element exposing (Element)
 import Html exposing(..)
 import Html.Attributes exposing (..)
 
 main : Html
 main =
+  div []
+    [ heading
+    , shapesImage
+    , sourceCodeLink
+    ]
+
+heading : Html
+heading =
+  h3
+    [ style
+        [ ("text-align", "center")
+        ]
+    ]
+    [ Html.text "Elixir, Phoenix, Elm, Dokku"
+    ]
+
+shapesImage : Html
+shapesImage =
   div
-    [ class "main-wrapper"
-    , style
+    [ style
         [ ("margin", "auto")
         , ("width", "400px")
         ]
     ]
-    [ h3
-        [ style
-          [ ("text-align", "center")
-          ]
+    [ fromElement
+        shapes
+    ]
+
+sourceCodeLink : Html
+sourceCodeLink =
+  ul
+    [ class "nav nav-pills"
+    , style
+        [ ("float", "right")
         ]
-        [ Html.text "Elixir, Phoenix, Elm, Dokku"
-        ]
-    , fromElement
-        (collage 400 200
-          [ ngon 4 70
-              |> filled clearBlue
-              |> move (00,0)
-          , ngon 5 50
-              |> filled clearGreen
-              |> move (60,30)
-          , ngon 3 60
-              |> filled clearRed
-              |> move (-60,-30)
-          ]
-        )
-    , ul
-        [ class "nav nav-pills"
-        , style
-            [ ("float", "right")
+    ]
+    [ li []
+        [ a
+            [ href "https://github.com/cmititiuc/elixir_test/tree/phoenix-elm-deploy"
             ]
-        ]
-        [ li []
-            [ a
-                [ href "https://github.com/cmititiuc/elixir_test/tree/phoenix-elm-deploy"
-                ]
-                [ Html.text "View Source Code"
-                ]
+            [ Html.text "View Source Code"
             ]
         ]
     ]
-
 
 clearBlue : Color
 clearBlue =
@@ -62,3 +64,16 @@ clearGreen =
 clearRed : Color
 clearRed =
   rgba 255 0 0 0.6
+
+shapes : Element
+shapes = collage 400 200
+  [ ngon 4 70
+      |> filled clearBlue
+      |> move (00,0)
+  , ngon 5 50
+      |> filled clearGreen
+      |> move (60,30)
+  , ngon 3 60
+      |> filled clearRed
+      |> move (-60,-30)
+  ]
