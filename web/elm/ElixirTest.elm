f801a5953e098cdbdcd0a4eacdaf00bea80f2f73
Catalin Mititiuc <catalin.mititiuc@gmail.com>
2016-03-27 14:40:49 -0400

    Better splash

--- web/elm/ElixirTest.elm
+++ web/elm/ElixirTest.elm
@@ -4,21 +4,47 @@ import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 import Text exposing (fromString)
+import Html exposing(..)
+import Html.Attributes exposing (..)
 
-main : Element
+main : Html
 main =
-  collage 700 200
-    [ ngon 4 75
-        |> filled clearGrey
-        |> move (-10,0)
-    , ngon 5 50
-        |> filled clearGrey
-        |> move (50,10)
-    , toForm(leftAligned (fromString "Hello from Elm"))
-        |> move (-100,50)
+  div
+    [ class "main-wrapper"
+    , style
+        [ ("margin", "auto")
+        , ("width", "400px")
+        ]
+    ]
+    [ h3
+        [ style
+          [ ("text-align", "center")
+          ]
+        ]
+        [ Html.text "Elixir, Phoenix, Elm, Dokku"
+        ]
+    , fromElement (collage 400 200
+        [ ngon 4 70
+            |> filled clearBlue
+            |> move (00,0)
+        , ngon 5 50
+            |> filled clearGreen
+            |> move (60,30)
+        , ngon 3 60
+            |> filled clearRed
+            |> move (-60, -30)
+        ])
     ]
 
 
-clearGrey : Color
-clearGrey =
-  rgba 111 111 111 0.6
+clearBlue : Color
+clearBlue =
+  rgba 0 0 255 0.6
+
+clearGreen : Color
+clearGreen =
+  rgba 0 255 0 0.6
+
+clearRed : Color
+clearRed =
+  rgba 255 0 0 0.6
