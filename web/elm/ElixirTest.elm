60c3469bce0eb20afd3c2e32a3dff77916c5c6c9
Catalin Mititiuc <catalin.mititiuc@gmail.com>
2016-03-27 12:13:31 -0400

    Make it prettier

--- web/elm/ElixirTest.elm
+++ web/elm/ElixirTest.elm
@@ -1,6 +1,24 @@
 module ElixirTest where
 
-import Html
+import Color exposing (..)
+import Graphics.Collage exposing (..)
+import Graphics.Element exposing (..)
+import Text exposing (fromString)
 
+main : Element
 main =
-  Html.text "Hello from Elm!"
+  collage 700 200
+    [ ngon 4 75
+        |> filled clearGrey
+        |> move (-10,0)
+    , ngon 5 50
+        |> filled clearGrey
+        |> move (50,10)
+    , toForm(leftAligned (fromString "Hello from Elm"))
+        |> move (-100,50)
+    ]
+
+
+clearGrey : Color
+clearGrey =
+  rgba 111 111 111 0.6
